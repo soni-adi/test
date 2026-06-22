47fcf506cf9c347305dfe5e4ca344871e43401fa
soni-adi <adityadev1021@gmail.com>
2026-06-23 00:26:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,1 @@
-import{defineConfig}from'vite';import react from'@vitejs/plugin-react';export default defineConfig({plugins:[react()],server:{port:5173,proxy:{'/api':{target:'http://localhost:5000',changeOrigin:true},'/uploads':{target:'http://localhost:5000',changeOrigin:true}}}})
+import{defineConfig}from'vite';import react from'@vitejs/plugin-react';export default defineConfig({plugins:[react()],server:{port:5173,proxy:{'/api':{target:'https://test-back-cu6f.onrender.com',changeOrigin:true},'/uploads':{target:'https://test-back-cu6f.onrender.com',changeOrigin:true}}}})
